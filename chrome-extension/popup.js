6a862bfd9ba72a632dfc474adf422ea6c28146d2
abuzar08 <ak.abuzar@gmail.com>
2023-04-01 17:03:47 -0400

    yay

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -22,12 +22,12 @@ chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
     args: [OPENAPI_KEY]
   });
 }).then(function (results) {
-  console.log(results)
+  // console.log(results)
 }).catch(function (error) {
-  console.log(error)
+  // console.log(error)
 });
 
-function onPageScript(api_key) {
+async function onPageScript(api_key) {
 
   function make_request(api_key, input_text) {
     const xhr = new XMLHttpRequest();
@@ -36,18 +36,18 @@ function onPageScript(api_key) {
     xhr.setRequestHeader("Authorization", "Bearer " + api_key)
     xhr.onload = () => {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        console.log(xhr.response);
+        // console.log(xhr.response);
       } else {
-        console.log(`Error: ${xhr.status}`);
+        // console.log(`Error: ${xhr.status}`);
       }
     };
 
     return new Promise(resolve => {
       xhr.onload = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log(xhr.response);
+          // console.log(xhr.response);
         } else {
-          console.log(`Error: ${xhr.status}`);
+          // console.log(`Error: ${xhr.status}`);
         }
         resolve({
           status: xhr.status,
@@ -66,34 +66,35 @@ function onPageScript(api_key) {
         "messages": [{"role": "user", "content": input_text}]
       }));
     })
-  }
+  };
   // Make a request to the LLM}
+  
   make_request(api_key, "The worlds most original youtube title").then(
     (result) => {
       console.log("Request finished")
       console.log(result)
   });
-
-  function replaceWithLLM(title) {
-    // Call the LLM for replacement here. 
-      const prompt = `I want you to change the title of this youtube video to not sound like clickbait anymore. But make sure not to change anything if the video title is of a music video. The title of the video is: ${title}`
-      const return_string = "This is a video."
-      return return_string
-    };
-
+  
   Array.from(
     document.querySelectorAll('[id="video-title"]'))
-    .forEach(function (x) {
+    .forEach(async function (x) {
         try{
           // const replacement = replaceWithLLM(x.innerHTML)
-          const replacement = replaceWithLLM(x.innerHTML)
-          x.innerHTML = replacement;
+          const text = x.innerHTML.toString()
+          const prompt = `I want you to change the title of this youtube video to not sound like clickbait anymore. But make sure not to change anything if the video title is of a music video. Don't use quotes and don't add any preamble to the start of the new title. The title of the video is: ${text}`
+          const replacement = await make_request(api_key, prompt)
+          // debugger;
+          const replacement_text = JSON.parse(replacement.response).choices[0].message.content
+          console.log("#################################")
+          console.log(text)
+          console.log(replacement_text)
+          x.innerHTML = replacement_text; 
         }
       catch(error){
         console.log(error)
       }
     }
-); 
+  );
 
   console.log("I'm on the page")
   return "xx"
